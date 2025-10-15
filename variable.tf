c5d6d654fbc6b5d345b25b9fd186cf1396892795
DevopsBalasai <chenibalasaivaraprasad@gmail.com>
2025-10-15 09:17:24 +0530

    jenkins

---
+++ variable.tf
@@ -0,0 +1,19 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_name" {
+  type        = string
+  default     = "roboshop.fun"
+  description = "description"
+}
+
+variable "zone_id" {
+  type        = string
+  default     = "Z04050822TDI959FELXIJ"
+  description = "description"
+}
